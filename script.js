a924f72c32aadee7c298551adedb0d4af99ec8e6
Rachel Fearon <rachel.fearon@gmail.com>
2021-09-12 10:52:08 -0400

    createTime function

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
-for (let i = 1; i <= 12; i++) {
-    let selector = document.getElementById("hours");
-    let option = document.createElement("OPTION");
 
-    selector.options.add(option);
-    option.text = i;
-    option.value = i;
+
+
+function createTime(hour, minute, ampm) {
+    if (minute.toString().length < 2) {
+        let minutes = `0${minute}`;
+        let time = moment(`${hour}:${minutes}:00 ${ampm}`, 'h:mm:ss A');
+        return time;
+    } else {
+        let minutes = minute;
+        let time = moment(`${hour}:${minutes}:00 ${ampm}`, 'h:mm:ss A');
+        return time;
+    }
+
 }
 
-for (let i = 1; i <= 59; i++) {
-    let selector = document.getElementById("minutes");
-    let option = document.createElement("OPTION");
 
-    selector.options.add(option);
-    if (i.toString().length < 2) {
-        option.text =  `${0}${i}`;
-        option.value = i;
-    } else {
+
+
+
+
+
+
+
+
+
+
+
+createTimeSelectors();
+//this won't work for each section - pass id into function?
+function createTimeSelectors() {
+    for (let i = 1; i <= 12; i++) {
+        let selector = document.getElementById("starterHours");
+        let option = document.createElement("OPTION");
+    
+        selector.options.add(option);
         option.text = i;
         option.value = i;
     }
     
+    for (let i = 1; i <= 59; i++) {
+        let selector = document.getElementById("starterMinutes");
+        let option = document.createElement("OPTION");
     
+        selector.options.add(option);
+        if (i.toString().length < 2) {
+            option.text =  `${0}${i}`;
+            option.value = i;
+        } else {
+            option.text = i;
+            option.value = i;
+        }
+    }
 }
+
+
 
 
 
